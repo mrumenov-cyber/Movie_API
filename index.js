ba2db22eac39f334085c378f90befac85ea0514d
mrumenov-cyber <marija.rumenovic@fesb.hr>
2021-11-23 19:44:11 +0100

    Exercise 3.5
    Changes in index.js, documentation added in documen tation.html, test.js created

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 //Declarations of the packages I am going to use
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  uuid = require ('uuid');
 
 const app = express(); //Encapsulated the express function with variable, app.
 
+//Index page request/route
 app.get('/', (req, res) => {
   res.send('Welcome to my Movie app!');
 });
 
+// Secret url path
 app.get('/secreturl', (req, res) => {
   res.send('This is a secret url with super top-secret content.');
 });
@@ -16,69 +20,117 @@ app.get('/secreturl', (req, res) => {
 let topMovies = [
     {
         title: 'Harry Potter and the Sorcerer\'s Stone',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2001,
+        director: ['David Yates', 'Alfonso Cuarón', 'Mike Newell', 'Chris Columbus'],
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
         title:'Harry Potter and the Chamber of Secrets',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2002,
+        director: ['David Yates', 'Alfonso Cuarón', 'Mike Newell', 'Chris Columbus'],
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
         title:'Harry Potter and the Prisoner of Azkaban',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2004,
+        director: ['David Yates', 'Alfonso Cuarón', 'Mike Newell', 'Chris Columbus'],
+        genre: ['Novel', 'Fantasy Fiction', 'Adventure']
     },
     {
         title:'Harry Potter and the Goblet of Fire',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2005,
+        director: ['David Yates', 'Alfonso Cuarón', 'Mike Newell', 'Chris Columbus'],
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
         title:'Harry Potter and the Order of the Phoneix',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2007,
+        director: ['David Yates', 'Alfonso Cuarón', 'Mike Newell', 'Chris Columbus'],
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
         title:'Harry Potter and the Half-Blood Prince',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2009,
+        director: ['David Yates', 'Alfonso Cuarón', 'Mike Newell', 'Chris Columbus'],
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
         title:'Harry Potter and the Deadly Hallows - Part 1',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2010,
+        director: ['David Yates', 'Alfonso Cuarón', 'Mike Newell', 'Chris Columbus'],
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
         title:'Harry Potter and the Deadly Hallows - Part 2',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2011,
+        director: ['David Yates', 'Alfonso Cuarón', 'Mike Newell', 'Chris Columbus'],
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
         title:'Wounderful Beasts and Where to Find them',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2016,
+        director: 'David Yates',
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
         title:'Wounderful Beasts and Where to Find them and Grindelwaldov\'s Evil Deeds',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2020,
+        director: 'David Yates',
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
         title:'Wounderful Beasts and Where to Find them and The Secrets of Dumbledore',
-        author: 'J:K: Rowling'
+        author: 'J:K: Rowling',
+        year: 2022,
+        director: 'David Yates',
+        genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
 ]
 
 // Midelware functions
-app.use(morgan('common')); //log all requests on terminal
-app.use(express.static('public')); //serve all static files in public folder
-
+app.use(morgan('common')); // log all requests on terminal
+app.use(express.static('public')); // serve all static files in public folder
+app.use(bodyParser.json()); //get required json data from http request body inside req handlers using req.body
 
 
 //GET requests
-app.get('/', (req, res) => {
-    res.send('Welcome to my book club!');
-});
 
+//Get documentation request/route
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', {root: __dirname});
 });
 
+//Get movies request/route
 app.get('/movies', (req, res) => {
-    res.json(topMovies);
+    res.json(topMovies); //return JSON object containing movies
 });
 
+//Get single movie by title
+app.get('/movies/:title', (req, res) => {
+    res.json(myMovies.find((movie) => {
+        return movie.titile === req.params.title
+    }));
+});
+
+//Get movies by genre
+app.get('/movies/genre/:title', (req, res) => {
+    res.send('GET request returning data about a movie genre.');
+});
+
+//Get movie director name
+app.get('movies/director/:name', (req, res) =>{
+    res.send('GET request that returs the name of movie director.')
+})
 
 // Created Error handler
 app.use((err, req, res, next) => {
