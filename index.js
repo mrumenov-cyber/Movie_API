59ce6457c14ffa28b1a6e314351341c7166f565c
mrumenov-cyber <marija.rumenovic@fesb.hr>
2021-12-02 00:49:19 +0100

    Exercise 3.8
    Functions written:
    -Return a list of ALL movies to the user
    -Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user
    -Return data about a genre (description) by name/title (e.g., “Thriller”)
    -Return data about a director (bio, birth year, death year) by name
    -Allow new users to register
    -Allow users to update their user info (username, password, email, date of birth)
    -Allow users to add a movie to their list of favorites
    -Allow users to remove a movie from their list of favorites
    -Allow existing users to deregister

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 const express = require('express'),
   morgan = require('morgan'),
   bodyParser = require('body-parser'),
-  uuid = require ('uuid');
+  uuid = require ('uuid'),
+  mongoose = require ('mongoose'),
+  Models = require('./models.js');
 
 const app = express(); //Encapsulated the express function with variable, app.
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect(
+    'mongodb://localhost:27017/myFlixDB', {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+}); //linking REST API  to mongodb database
 
 //Index page request/route
 app.get('/', (req, res) => {
@@ -75,21 +85,21 @@ let topMovies = [
         genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
-        title:'Wounderful Beasts and Where to Find them',
+        title:'Fantastic Beasts and Where to Find them',
         author: 'J:K: Rowling',
         year: 2016,
         director: 'David Yates',
         genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
-        title:'Wounderful Beasts and Where to Find them and Grindelwaldov\'s Evil Deeds',
+        title:'Fantastic Beasts and Where to Find them and Grindelwaldov\'s Evil Deeds',
         author: 'J:K: Rowling',
         year: 2020,
         director: 'David Yates',
         genre: ['Novel', 'Fantasy Fiction', 'Children\'s movie', 'Adventure']
     },
     {
-        title:'Wounderful Beasts and Where to Find them and The Secrets of Dumbledore',
+        title:'Fantastic Beasts and Where to Find them and The Secrets of Dumbledore',
         author: 'J:K: Rowling',
         year: 2022,
         director: 'David Yates',
@@ -106,7 +116,7 @@ app.use(bodyParser.json()); //get required json data from http request body insi
 //GET requests
 
 //Get documentation request/route
-app.get('/documentation', (req, res) => {
+app.get('/documentations', (req, res) => {
     res.sendFile('public/documentation.html', {root: __dirname});
 });
 
@@ -117,20 +127,147 @@ app.get('/movies', (req, res) => {
 
 //Get single movie by title
 app.get('/movies/:title', (req, res) => {
-    res.json(myMovies.find((movie) => {
-        return movie.titile === req.params.title
+    res.json(topMovies.find((movie) => {
+        return movie.title === req.params.title
     }));
 });
 
 //Get movies by genre
-app.get('/movies/genre/:title', (req, res) => {
-    res.send('GET request returning data about a movie genre.');
+app.get('/movies/genre/:name', (req, res) => {
+    Movies.findOne({ "Genre.Name" : req.params.name })
+      .then((genre) => {
+        res.json(genre);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
+
+//Gets information about a specific director
+app.get('/movies/director/:name', (req, res) => {
+    Movies.findOne({ "Director.Name" : req.params.name })
+      .then((director) => {
+        res.json(director);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
+  
+  app.post('/users', (req, res) => {
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: req.body.Password,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
+
+//Get all users
+app.get('/users', (req, res) => {
+    Users.find()
+    .then((users) => {
+        res.status(201).json(users);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500). send('Error: ' + err);
+    });
 });
 
-//Get movie director name
-app.get('movies/director/:name', (req, res) =>{
-    res.send('GET request that returs the name of movie director.')
-})
+app.get('/users/:Username', (req, res) => 
+    {
+    Users.findOne({Username: req.params.Username})
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+// Update a user's info, by username
+/* We’ll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+app.put('/users/:Username', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+      {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      }
+    },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if(err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+  });
+
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+       $push: { FavoriteMovies: req.params.MovieID }
+     },
+     { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+  });
+  
+ // Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+      .then((user) => {
+        if (!user) {
+          res.status(400).send(req.params.Username + ' was not found');
+        } else {
+          res.status(200).send(req.params.Username + ' was deleted.');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });  
 
 // Created Error handler
 app.use((err, req, res, next) => {
