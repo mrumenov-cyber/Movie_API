71d62e7a052fb8f186c1550a74f0f16d475116dc
mrumenov-cyber <marija.rumenovic@fesb.hr>
2021-12-14 15:06:36 +0100

    Removed authentification to get movies with react myFlix app

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.get('/documentations', (req, res) => {
 });
 
 //Get movies request/route
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
